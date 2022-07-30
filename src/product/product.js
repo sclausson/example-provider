463d74a0b44129675fff916a084a595553c320a9
Shayne Clausson <shayne.clausson@extendaretail.com>
2022-07-30 21:30:58 +0200

    Break by setting id to uuid

--- src/product/product.js
+++ src/product/product.js
@@ -1,6 +1,6 @@
 class Product {
     constructor(id, type, name, version) {
-        this.id = id;
+        this.uuid = id;
         this.type = type;
         this.name = name;
         this.version = version;
